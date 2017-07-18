c480602cbd5bb33094542832c4ba3fc971c943eb
susham kumar <yerabolu@pdx.edu>
2017-07-18 16:37:41 -0700

    GUI Screen code and calling the FTPClient.connect method from GUI

--- src/edu/pdx/cs510/agile/team3/Main.java
+++ src/edu/pdx/cs510/agile/team3/Main.java
@@ -1,9 +1,20 @@
 package edu.pdx.cs510.agile.team3;
 
+
+import edu.pdx.cs510.agile.team3.FTP.CLIClient;
+import edu.pdx.cs510.agile.team3.FTP.ServerConnect;
+
 public class Main {
 
     public static void main(String[] args) {
-        CLIClient cliClient = new CLIClient();
+
+
+        ServerConnect serverConnect= new ServerConnect();
+        serverConnect.setVisible(true);
+        CLIClient cliClient= new CLIClient();
+
+
+
         cliClient.start(args);
     }
 }
