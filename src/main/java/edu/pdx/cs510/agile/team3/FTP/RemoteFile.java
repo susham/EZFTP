126bbbeb38290306a9576b42af566dc3725057af
Henry <hacoo36@gmail.com>
2017-07-22 14:22:54 -0700

    Added CLI option to list remote directory

--- src/main/java/edu/pdx/cs510/agile/team3/FTP/RemoteFile.java
+++ src/main/java/edu/pdx/cs510/agile/team3/FTP/RemoteFile.java
@@ -3,5 +3,13 @@ package edu.pdx.cs510.agile.team3.FTP;
 /**
  * Created by henry on 7/22/17.
  */
-public class RemoteFile {
+public class RemoteFile extends FileBase {
+    public RemoteFile() {
+        super();
+    }
+
+    public RemoteFile(String fileName, String filePath, boolean directoryFlag) {
+        super(fileName, filePath, directoryFlag, true);
+    }
+
 }
