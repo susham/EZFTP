0851d578141eab594d917109326a1cc08b499f45
Henry <hacoo36@gmail.com>
2017-07-22 16:22:05 -0700

    Added example JUnit test

--- src/test/java/JUnitTest.java
+++ src/test/java/JUnitTest.java
@@ -1,6 +1,7 @@
-package edu.pdx.cs510.agile.team3.FTP;
-
+import org.junit.Assert;
 import org.junit.Test;
+
+import edu.pdx.cs510.agile.team3.FTP.*;
 /**
  * Created by henry on 7/22/17.
  *
@@ -9,6 +10,8 @@ import org.junit.Test;
 public class JUnitTest {
     @Test
     public void jUnitTest() {
+        Assert.assertEquals("FAIL - this test should never fail!", 0, 0);
+        //Assert.assertEquals("OK - this test should always FAIL!", 0, 1);
         System.out.println("I'm a test output. If JUnit is working, you should see me.");
     }
 }
