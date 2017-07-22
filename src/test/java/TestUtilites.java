9706db4220162cf39cd14210f69cebcae2364dd6
Henry <hacoo36@gmail.com>
2017-07-22 16:22:19 -0700

    Added test utility class

--- src/test/java/TestUtilites.java
+++ src/test/java/TestUtilites.java
@@ -1,7 +1,22 @@
-package PACKAGE_NAME;
+import edu.pdx.cs510.agile.team3.FTP.*;
 
 /**
  * Created by henry on 7/22/17.
+ *
+ * Utility functions for testing
  */
 public class TestUtilites {
+
+    // Return info for the test server
+    public static FTPServerInfo getTestServerInfo() {
+        FTPServerInfo serverInfo = new FTPServerInfo(
+                "Test server",
+                "138.68.11.232",
+                "agile",
+                "imanagiledude",
+                21
+        );
+        return serverInfo;
+    }
+
 }
